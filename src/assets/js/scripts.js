b83893cc16f8e23f9fe86f9c123ec3d69717a226
Ben Collins <benicoll@hotmail.co.uk>
2019-10-11 07:46:41 +0100

    Tidy comments from JS

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -1,4 +1,3 @@
-//loop through all the answers and place them hidden into the dom
 for(var i = 0; i<answers.length; i++){
 	var answerRow = '<div class="answer" id="' + stripWhitespace(answers[i].value) + '">' +
 	                '    <div class="answer__number">' + parseInt(i + 1) + '</div>' +
@@ -9,11 +8,9 @@ for(var i = 0; i<answers.length; i++){
 	$('#answerTable').append(answerRow);
 }
 
-//create arrays for incorrect and correct guesses that are either blank or collected from local storage
 var incorrectGuesses = JSON.parse(localStorage.getItem('incorrectGuesses') || "[]");
 var correctGuesses = JSON.parse(localStorage.getItem('correctGuesses') || "[]");
 
-//if there are localstorage values, display the already guessed answers on the scoreboard
 if((incorrectGuesses.length > 0) || (correctGuesses.length > 0)) {
 	revealGuesses();
 }
@@ -31,12 +28,7 @@ $('form').submit(function(e){
 			correctGuesses.push(guess);
 			localStorage.setItem('correctGuesses', JSON.stringify(correctGuesses));
 
-			$('#' + stripWhitespace(guess) + ' span').show(); //show guess on the scoreboard
-
-			//check to see if top answer
-			// if(answers[0].value == guess){
-			// 	console.log('top answer');
-			// }
+			$('#' + stripWhitespace(guess) + ' span').show();
 
 		} else {
 			wrongAudio.play();
@@ -45,9 +37,8 @@ $('form').submit(function(e){
 
 			showCross();
 
-			$('ul#guessList').append('<li>' + guess + '</li>'); //add the guess to the incorrect guess list
+			$('ul#guessList').append('<li>' + guess + '</li>');
 
-			//if theres more than 3 guesses, disable the form and show the 'show answers' button
 			if(incorrectGuesses.length >= 3){
 				$('form input#submit').prop("disabled", true);
 				$('#showAnswers').show();
@@ -55,13 +46,12 @@ $('form').submit(function(e){
 		}
 	}
 
-	$input.val(''); //empty the input field after submit clicked
+	$input.val('');
 });
 
 function checkAnswer(guess){
 	var exists = false;
 
-	//loop through the answers array to see if guess exists
 	for(var i = 0; i<answers.length; i++){
 		if(answers[i].value.toLowerCase() == guess){
 			exists = true;
@@ -74,29 +64,23 @@ function checkAnswer(guess){
 
 function showCross(number){
 	if(number == null){
-		//if theres no number specified show first hidden cross.
 		$('#crosses img.hidden').first().removeClass('hidden').addClass('show');
 	}else{
-		//if there is, show the passed number of crosses
 		$('#crosses img.hidden').slice(0,number).removeClass('hidden').addClass('show');
 	}
 }
 
 function revealGuesses(){
-	//loop through incorrect guesses and add it to the guess list
 	for(var i = 0; i<incorrectGuesses.length; i++){
 		$('ul#guessList').append('<li>' + incorrectGuesses[i] + '</li>');
 	}
 
-	//loop through correct guesses and show them in the scoreboard
 	for(var c = 0; c<correctGuesses.length; c++){
 		$('#' + correctGuesses[c] + ' span').show();
 	}
 
-	//show the correct number of crosses
 	showCross(incorrectGuesses.length);
 
-	//disable the input and show the 'show answers' button if 3 incorrect guesses
 	if(incorrectGuesses.length >= 3){
 		$('#showAnswers').show();
 		$('form input#submit').prop("disabled", true);
@@ -107,7 +91,6 @@ function stripWhitespace(sentence) {
 	return sentence.replace(/\s/g, '');
 }
 
-//remove localstorage items and refresh the browser
 $('#clear').on('click', function(){
 	localStorage.removeItem('incorrectGuesses');
 	localStorage.removeItem('correctGuesses');
@@ -115,7 +98,6 @@ $('#clear').on('click', function(){
 	location.reload();
 });
 
-//show all answers
 $('#showAnswers').on('click', function(){
 	$('.answer span').show();
 });
